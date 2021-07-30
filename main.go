39aa3c7600f2a089058b823834d8305b3d0401dd
burrell <eewinterz@gmail.com>
2021-07-29 21:58:45 -0400

    adding functionality for creating a item

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 var nextId int = 0
@@ -18,9 +19,21 @@ func GetTodos(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"list": todos})
 }
 
+func PostTodo(c *gin.Context) {
+	var item Todo
+	if err := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	item.Id = GetNextId()
+	todos = append(todos, item)
+	c.String(http.StatusCreated, c.FullPath() + "/" + strconv.Itoa(item.Id))
+}
+
 func main() {
 	todos = append(todos, Todo{Id: GetNextId(), Value: "CodeHouse", DueDate: "7/31/2021"})
 	r := gin.Default()
 	r.GET("/api/todos", GetTodos)
+	r.POST("/api/todos", PostTodo)
 	r.Run(":8090")
 }
