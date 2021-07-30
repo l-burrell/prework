a0627a26a595a1f76533975938a579166e7bb5ce
burrell <eewinterz@gmail.com>
2021-07-29 21:11:16 -0400

    getting the todo items

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main 
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
+var nextId int = 0
+var todos []Todo
+
+func GetNextId() int {
+	value := nextId
+	nextId++
+	return value
+}
+
+func GetTodos(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"list": todos})
+}
+
 func main() {
+	todos = append(todos, Todo{Id: GetNextId(), Value: "CodeHouse", DueDate: "7/31/2021"})
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Welcome to the world of Go and Gin")
-	})
+	r.GET("/api/todos", GetTodos)
 	r.Run(":8090")
 }
