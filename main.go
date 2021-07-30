1731746971a4b0edaeb97045f5a3a142acc13dfe
burrell <eewinterz@gmail.com>
2021-07-30 00:43:18 -0400

    setup the vue frontend

--- main.go
+++ main.go
@@ -30,10 +30,26 @@ func PostTodo(c *gin.Context) {
 	c.String(http.StatusCreated, c.FullPath() + "/" + strconv.Itoa(item.Id))
 }
 
+func DeleteTodo(c *gin.Context) {
+	idString := c.Param("id")
+
+	if id, err := strconv.Atoi(idString); err == nil {
+		for index := range todos {
+			if todos[index].Id == id {
+				todos = append(todos[:index], todos[index+1:]...)
+				c.Writer.WriteHeader(http.StatusNoContent)
+				return
+			}
+		}
+	}
+	c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+}
+
 func main() {
 	todos = append(todos, Todo{Id: GetNextId(), Value: "CodeHouse", DueDate: "7/31/2021"})
 	r := gin.Default()
 	r.GET("/api/todos", GetTodos)
 	r.POST("/api/todos", PostTodo)
+	r.DELETE("/api/todos/:id", DeleteTodo)
 	r.Run(":8090")
 }
