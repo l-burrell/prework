fdd3bed613d5e8afd032f487698d9d1eb14caaac
burrell <eewinterz@gmail.com>
2021-07-30 07:54:33 -0400

    style the todo list

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <todo-list :todos="todos"/>
+    <todo-form @add:todo="addTodo" />
+    <todo-list :todos="todos" />
   </div>
 </template>
 
 <script>
+import TodoForm from '@/components/TodoForm.vue'
 import TodoList from '@/components/TodoList.vue'
 
 const appData = {
   todos: []
 }
-
+  
 export default {
   name: 'app',
   components: {
+    TodoForm,
     TodoList
   },
   data() {
@@ -23,25 +26,42 @@ export default {
     this.getTodos();
   },
   methods: {
-    getTodos: getTodos
+    getTodos: getTodos,
+    addTodo: addTodo
   }
 }
 
 async function getTodos() {
+    try {
+      const response = await fetch('api/todos')
+      const data = await response.json()
+      appData.todos = data.list
+    } 
+    catch (error) {
+      console.error(error)
+    }
+}
+
+async function addTodo(todo) {
   try {
-    const response = await fetch('api/todos')
-    const data = await response.json()
-    appData.todos = data.list
+    const response = await fetch('/api/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+    console.log(response.status)
+    await getTodos()
   } 
   catch (error) {
     console.error(error)
   }
 }
+
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  margin-top: 60px;
+  margin-top: 50px;
 }
 </style>
